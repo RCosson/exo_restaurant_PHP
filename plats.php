fcbd5ee75f8a833de8e858ef8eb43aac84c08ccf
RCosson <romain.cosson11@live.fr>
2017-06-20 11:32:45 +0200

    php en pdo

--- plats.php
+++ plats.php
@@ -10,21 +10,27 @@
 
 <?php
 
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password="?onepiece?"; // Mysql password
-$db_name="restaurant"; // Database name
-// Connect to server and select databse.
-$dbconn = mysqli_connect($host, $username, $password)or die("cannot connect");
-mysqli_select_db($dbconn, $db_name)or die("cannot select DB");
+try
+{
+  $bdd = new PDO('mysql:host=localhost;dbname=restaurant;charset=utf8', 'root', '?onepiece?');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
 
 $nom = $_POST['nom'];
 $prix = $_POST['prix'];
 $photo = $_POST['photo'];
 
-$query = "INSERT INTO `plats` ( `nom`, `prix`, `image`) VALUES ('$nom', '$prix','$photo')";
+$req = $bdd->prepare("INSERT INTO `plats` ( `nom`, `prix`, `image`) VALUES ('$nom', '$prix','$photo')");
+$req->execute(array(
+	'nom' => $nom,
+	'prix' => $prix,
+	'photo' => $photo,
+	));
 
-mysqli_query($dbconn, $query);
+echo 'Le plat a bien été ajouté !';
 
 ?>
 
